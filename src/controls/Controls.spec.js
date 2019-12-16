88f99b1b7c3c4a557b1e8b46a82459c9758d0f3b
josh Hill <Josh_hill15@me.com>
2019-12-16 18:39:11 -0500

    mvp

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,27 @@
 // Test away!
+import React from "react"
+import { render, fireEvent } from "@testing-library/react"
+import Controls from "./Controls"
+
+test("component renders", () => {
+    const tree = render(<Controls />)
+    expect(tree)
+})
+
+test("expect open gate text after the gate is closed", () => {
+    setTimeout(() => {
+        const tree = render(<Controls locked = {false} closed = {false} />)
+        const button = tree.getByText(/close gate/i)
+        fireEvent.click(button)
+        tree.getByText(/open gate/i)
+    }, 1000)
+})
+
+test("expect closed gate text after the gate is open", () => {
+    setTimeout(() => {
+        const tree = render(<Controls locked = {true} closed = {true} />)
+        const button = tree.getByText(/open gate/i)
+        fireEvent.click(button)
+        tree.getByText(/close gate/i)
+    }, 1000)
+})
